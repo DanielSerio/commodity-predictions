2bf3723063149a1b8e21349e0f6ebf228078827c
DanielSerio <dserio.d@gmail.com>
2026-02-14 09:12:22 -0500

    pr: fix params type

--- app/predictions/[id]/page.tsx
+++ app/predictions/[id]/page.tsx
@@ -6,12 +6,8 @@ export const metadata = {
   description: 'Validate and adjust AI commodity forecasts.',
 };
 
-export default async function Page({
-  params,
-}: {
-  params: Promise<{ id: string }>;
-}) {
-  const { id } = await params;
+export default async function Page({ params }: { params: { id: string } }) {
+  const { id } = params;
   const predictionId = parseInt(id);
 
   if (isNaN(predictionId)) {
